76109909be23ef6271b31cf1a6a31d2f20f48881
Stefanos Charalambous <2315792c@student.gla.ac.uk>
2020-07-16 18:19:16 +0100

    Get Current User Profile

--- models/Profile.js
+++ models/Profile.js
@@ -111,4 +111,4 @@ const ProfileSchema = new mongoose.Schema({
   },
 })
 
-modules.exports = Profile = mongoose.model('profile', ProfileSchema)
+module.exports = Profile = mongoose.model('profile', ProfileSchema)
