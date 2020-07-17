dad9cb504e87b4384b8e55204232f73463c87f51
Stefanos Charalambous <2315792c@student.gla.ac.uk>
2020-07-17 10:57:04 +0100

    Delete Profile Experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -165,6 +165,7 @@ router.delete('/', auth, async (req, res) => {
 // @route    PUT api/profile/experience
 // @desc     Add profile experience
 // @access   Private
+
 router.put(
   '/experience',
   [
@@ -215,5 +216,29 @@ router.put(
     }
   }
 )
+
+// @route    DELETE api/profile/experience/:exp_id
+// @desc     Delete experience from profile
+// @access   Private
+
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id })
+
+    // Get remove index
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id)
+
+    profile.experience.splice(removeIndex, 1)
+
+    await profile.save()
+
+    res.json(profile)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+})
 
 module.exports = router
