2f3a86e732518969f224134ec190ca0277fa907a
Stefanos Charalambous <2315792c@student.gla.ac.uk>
2020-07-26 14:29:20 +0100

    Auth Reducer & Register Action

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
+import { REGISTER_FAIL, REGISTER_SUCCESS } from './types'
 import { setAlert } from './alert'
-import { REGISTER_SUCCESS, REGISTER_FAIL } from './types'
 
 // Register User
 export const register = ({ name, email, password }) => async (dispatch) => {
   const config = {
-    header: {
+    headers: {
       'Content-Type': 'application/json',
     },
   }
@@ -26,8 +26,6 @@ export const register = ({ name, email, password }) => async (dispatch) => {
       errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')))
     }
 
-    dispatch({
-      type: REGISTER_FAIL,
-    })
+    dispatch({ type: REGISTER_FAIL })
   }
 }
