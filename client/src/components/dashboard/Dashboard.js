c2bfe2e26c30ac48b4e55063b4a149a07a2bf0d6
Stefanos Charalambous <charstefanos@gmail.com>
2020-07-28 13:25:38 +0100

    Starting On The Dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { useEffect, Fragment } from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
@@ -23,7 +24,12 @@ const Dashboard = ({
       {profile !== null ? (
         <Fragment>has</Fragment>
       ) : (
-        <Fragment>has not</Fragment>
+        <Fragment>
+          <p>You have not yet setup a profile, please add some info</p>
+          <Link to="/create-profile" className="btn btn-primary my-1">
+            Create Profile
+          </Link>
+        </Fragment>
       )}
     </Fragment>
   )
@@ -37,7 +43,7 @@ Dashboard.propTypes = {
 
 const mapStateToProps = (state) => ({
   auth: state.auth,
-  profile: state.auth,
+  profile: state.profile,
 })
 
 export default connect(mapStateToProps, { getCurrentProfile })(Dashboard)
